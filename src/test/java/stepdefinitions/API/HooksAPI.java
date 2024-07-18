81971ecfb408ad2c4f11a100abd363c7f91af09f
ercan <etogrul02@gmail.com>
2024-07-18 09:19:32 +0200

    Modul7 OtomotisirungTestfälle schreibe 18.07.2024

--- src/test/java/stepdefinitions/API/HooksAPI.java
+++ src/test/java/stepdefinitions/API/HooksAPI.java
@@ -6,8 +6,8 @@ import static base_urls.Gm3BaseUrl.setSpec;
 
 public class HooksAPI {
 
-    @Before("@Api3")// tüm senoriolar öncesi calisir
-    public static void setUp(){
+    @Before("@Api2")// tüm senoriolar öncesi calisir
+    public static void setUp() {
         setSpec();// spec objesi null kalmamasi icin her senorya öncesi cagirilir.
 
 
