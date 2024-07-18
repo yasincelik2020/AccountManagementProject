0098dbfa55cbdad6323589fa147dbfa00f1dcf46
beyza <csrbyz@gmail.com>
2024-07-18 14:44:32 +0200

    Role Service

--- src/test/java/stepdefinitions/API/RoleServiceSD.java
+++ src/test/java/stepdefinitions/API/RoleServiceSD.java
@@ -12,53 +12,131 @@ import static org.junit.Assert.assertEquals;
 import static stepdefinitions.API.HooksAPI.setUp;
 import static stepdefinitions.API.UserInfo.*;
 
+import io.restassured.path.json.JsonPath;
+
+import java.util.List;
+
 public class RoleServiceSD {
 
     Response response;
-// 6_1_2
-    @Given("Einstellen url für Role Service")
-    public void einstellen_url_für_role_service() {
-        setUp();
-        spec.pathParams("erste", "v1","zweite","name","dritte",appID);
 
+    // 6_1_2
+
+    @Given("Einstellen url für Role Service\\(appOrgRole)")
+    public void einstellen_url_für_role_service_app_org_role() {
+        setUp();
+        spec.pathParams("erste", "application", "zweite", appID, "dritte", "organization", "vierte", org_id, "fünfte", "role");
     }
-    @When("Der Benutzer sendet eine Anfrage mit der GET-Methode für Role Service")
-    public void der_benutzer_sendet_eine_anfrage_mit_der_get_methode_für_role_service() {
-    // ???    response = RestAssured.given(spec).get("{erste}");
+
+    @When("Der Benutzer sendet eine Anfrage mit der GET-Methode für Role Service\\(appOrgRole)")
+    public void der_benutzer_sendet_eine_anfrage_mit_der_get_methode_für_role_service_app_org_role() {
+        response = RestAssured
+                .given(spec)
+                .get("{erste}/{zweite}/{dritte}/{vierte}/{fünfte}");
+
         response.prettyPrint();
 
     }
-    @Then("Der Statuscode {int} wird bestätigt für Role Service")
-    public void der_statuscode_wird_bestätigt_für_role_service(Integer statusCode) {
-        assertEquals(200,response.statusCode());
+
+    @Then("Der Statuscode {int} wird bestätigt für Role Service\\(appOrgRole)")
+    public void der_statuscode_wird_bestätigt_für_role_service_app_org_role(Integer int1) {
+        assertEquals(200, response.statusCode());
 
     }
-    @Then("Der Name wird bestätigt für Role Service")
-    public void der_name_wird_bestätigt_für_role_service() {
-        response.then().body("name",equalTo("Accountant"));
 
+    @Then("Der Name wird bestätigt für Role Service\\(appOrgRole)")
+    public void der_name_wird_bestätigt_für_role_service_app_org_role() {
+        JsonPath jsonPath = response.jsonPath(); //Response'ı JsonPath'e çevirdik
+        List<String> list = jsonPath.getList("findAll{it.id==23}.name");
+        System.out.println("list.getFirst() = " + list.get(0));
 
+        assert list.get(0).equals("Accountant");
     }
 
 // 6_3_4
 
+    @Given("Einstellen url für Role Service\\(appRole)")
+    public void einstellen_url_für_role_service_app_role() {
+        setUp();
+        spec.pathParams("erste", "application", "zweite", appID, "dritte","role");
+
+    }
+
+    @When("Der Benutzer sendet eine Anfrage mit der GET-Methode für Role Service\\(appRole)")
+    public void der_benutzer_sendet_eine_anfrage_mit_der_get_methode_für_role_service_app_role() {
+        response = RestAssured
+                .given(spec)
+                .get("{erste}/{zweite}/{dritte}");
+
+        response.prettyPrint();
+
+    }
+    @Then("Der Statuscode {int} wird bestätigt für Role Service\\(appRole)")
+    public void der_statuscode_wird_bestätigt_für_role_service_app_role(Integer int1) {
+        assertEquals(200, response.statusCode());
+
+    }
+    @Then("Der Name wird bestätigt für Role Service\\(appRole)")
+    public void der_name_wird_bestätigt_für_role_service_app_role() {
+        JsonPath jsonPath = response.jsonPath(); //Response'ı JsonPath'e çevirdik
+        List<String> list = jsonPath.getList("findAll{it.id==4}.name");
+        System.out.println("list.getSecond() = " + list.get(0));
+
+        assert list.get(0).equals("APP_DOMAIN_MANAGER");
+    }
+
 // 6_5_6
 
-    @Then("Die app_id wird bestätigt für Role Service")
-    public void die_app_id_wird_bestätigt_für_role_service() {
+    @Given("Einstellen url für Role Service\\(role)")
+    public void einstellen_url_für_role_service_role() {
+        setUp();
+        spec.pathParams("erste", "role");
+    }
+    @When("Der Benutzer sendet eine Anfrage mit der GET-Methode für Role Service\\(role)")
+    public void der_benutzer_sendet_eine_anfrage_mit_der_get_methode_für_role_service_role() {
+        response = RestAssured
+                .given(spec)
+                .get("{erste}");
+        response.prettyPrint();
+
+    }
+    @Then("Der Statuscode {int} wird bestätigt für Role Service\\(role)")
+    public void der_statuscode_wird_bestätigt_für_role_service_role(Integer int1) {
+        assertEquals(200, response.statusCode());
+
+    }
+    @Then("Die app_id wird bestätigt für Role Service\\(role)")
+    public void die_app_id_wird_bestätigt_für_role_service_role() {
         response.then().body("app_id",equalTo(2));
     }
 
 // 6_7_8
 
-    @Then("Die Id des ersten Elements von permission wird bestätigt.")
-    public void die_ıd_des_ersten_elements_von_permission_wird_bestätigt() {
-        response.then().body("permission[0].id",equalTo("493"));
+    @Given("Einstellen url für Role Service\\(roleID)")
+    public void einstellen_url_für_role_service_role_ıd() {
+        setUp();
+        spec.pathParams("erste", "role","zweite","role_id");
 
     }
+    @When("Der Benutzer sendet eine Anfrage mit der GET-Methode für Role Service\\(roleID)")
+    public void der_benutzer_sendet_eine_anfrage_mit_der_get_methode_für_role_service_role_ıd() {
+        response = RestAssured
+                .given(spec)
+                .get("{erste}/{zweite}");
 
+        response.prettyPrint();
 
+    }
+    @Then("Der Statuscode {int} wird bestätigt für Role Service\\(roleID)")
+    public void der_statuscode_wird_bestätigt_für_role_service_role_ıd(Integer int1) {
+        assertEquals(200, response.statusCode());
 
+    }
+    @Then("Die Id des ersten Elements von permission wird bestätigt.\\(roleID)")
+    public void die_ıd_des_ersten_elements_von_permission_wird_bestätigt_role_ıd() {
+        response.then().body("permission[0].id", equalTo("493"));
+
+    }
 
 
 }
--- src/test/resources/featuresAPI/06_RoleService.feature
+++ src/test/resources/featuresAPI/06_RoleService.feature
@@ -1,4 +1,4 @@
-@Api @Modul06
+@Modul06
 Feature:Get Organization Service
 
   Background: Get User Info
@@ -7,28 +7,28 @@ Feature:Get Organization Service
     And get the user info information
 
   Scenario:M06_TC_01_02_ Get Role Service Scenario (AppOrgRole)
-    Given Einstellen url für Role Service
-    When Der Benutzer sendet eine Anfrage mit der GET-Methode für Role Service
-    Then Der Statuscode 200 wird bestätigt für Role Service
-    And Der Name wird bestätigt für Role Service
+    Given Einstellen url für Role Service(appOrgRole)
+    When Der Benutzer sendet eine Anfrage mit der GET-Methode für Role Service(appOrgRole)
+    Then Der Statuscode 200 wird bestätigt für Role Service(appOrgRole)
+    And Der Name wird bestätigt für Role Service(appOrgRole)
 
   Scenario:M06_TC_03_04_ Get Role Service Scenario (AppRole)
-    Given Einstellen url für Role Service
-    When Der Benutzer sendet eine Anfrage mit der GET-Methode für Role Service
-    Then Der Statuscode 200 wird bestätigt für Role Service
-    And Der Name wird bestätigt für Role Service
+    Given Einstellen url für Role Service(appRole)
+    When Der Benutzer sendet eine Anfrage mit der GET-Methode für Role Service(appRole)
+    Then Der Statuscode 200 wird bestätigt für Role Service(appRole)
+    And Der Name wird bestätigt für Role Service(appRole)
 
   Scenario:M06_TC_05_06_ Get Role Service Scenario (Role)
-    Given Einstellen url für Role Service
-    When Der Benutzer sendet eine Anfrage mit der GET-Methode für Role Service
-    Then Der Statuscode 200 wird bestätigt für Role Service
-    And Die app_id wird bestätigt für Role Service
+    Given Einstellen url für Role Service(role)
+    When Der Benutzer sendet eine Anfrage mit der GET-Methode für Role Service(role)
+    Then Der Statuscode 200 wird bestätigt für Role Service(role)
+    And Die app_id wird bestätigt für Role Service(role)
 
   Scenario:M06_TC_07_08_ Get Role Service Scenario (RoleRoleId)
-    Given Einstellen url für Role Service
-    When Der Benutzer sendet eine Anfrage mit der GET-Methode für Role Service
-    Then Der Statuscode 200 wird bestätigt für Role Service
-    And Die Id des ersten Elements von permission wird bestätigt.
+    Given Einstellen url für Role Service(roleID)
+    When Der Benutzer sendet eine Anfrage mit der GET-Methode für Role Service(roleID)
+    Then Der Statuscode 200 wird bestätigt für Role Service(roleID)
+    And Die Id des ersten Elements von permission wird bestätigt.(roleID)
 
 
 
